a4cd9e76d99e523f1b1d94e36701640274d59cbe
soulchild <soulchildsince2013@gmail.com>
2017-05-08 00:07:52 +0800

    Initial Commit

---
+++ fateSegue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fateSegue
+//
+//  Created by Soul on 08/05/2017.
+//  Copyright © 2017 sweatshops. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
